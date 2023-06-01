75f6193be348ddabf4ba480dfb5ee30834ce417e
M. Haykal <haykalmuhammad456@gmail.com>
2023-06-01 15:38:53 +0700

    tulisan tranform

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const text = "Selamat datang di HayShoes.";
-const typingInterval = 100; // Jeda antara karakter (dalam milidetik)
-const repeatInterval = 300; // Jeda antara setiap pengulangan (dalam milidetik)
-
-function typeText() {
-    const typingText = document.getElementById('typing-text');
-    typingText.innerHTML = '';
-
-    let index = 0;
-
-    function typeNextCharacter() {
-        if (index < text.length) {
-            typingText.innerHTML += text[index];
-            index++;
-            setTimeout(typeNextCharacter, typingInterval);
-        } else {
-            setTimeout(typeText, repeatInterval);
-        }
-    }
-
-    typeNextCharacter();
-}
-
-typeText();
-
 const form = document.getElementById("comment-form");
 const nameInput = document.getElementById("name");
 const commentInput = document.getElementById("comment");
